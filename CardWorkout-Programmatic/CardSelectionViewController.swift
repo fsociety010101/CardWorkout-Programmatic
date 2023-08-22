0b5b9ab3749436e6470edf38e6be1b7db9786e58
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-08-22 17:21:49 +0200

    added programmatic ui func

---
+++ CardWorkout-Programmatic/CardSelectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CardSelectionViewController.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Dmytro Nimchynskyi on 22/08/2023.
+//
+
+import UIKit
+
+class CardSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .brown
+    }
+}
